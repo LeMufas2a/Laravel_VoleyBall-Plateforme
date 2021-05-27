5cff0cdbdd0cccf039f7608c6d53e63abb954555
LeMufas2a <codage.mufasa@gmail.com>
2021-05-27 06:36:53 +0200

    create ok

--- resources/views/backoffice/equipe/create.blade.php
+++ resources/views/backoffice/equipe/create.blade.php
@@ -3,43 +3,113 @@
 @section('content')
 @include('partials.nav')
 <main>
-    <div class="container w-25 text-white">
+    <div class="container w-100  text-white">
         <h1 class="text-center my-5">Create Equipes</h1>
-        <form action={{ route('equipe.store') }} method="post" enctype="multipart/form-data">
+        <ul class="w-100">
+            @foreach ($errors->all() as $message)
+            <li class="text-white  text-center" style="list-style: none"><i
+                    class="fas fa-exclamation-triangle"></i>{{$message}}</li>
+            @endforeach
+        </ul>
+        <form class=" d-flex w-100 justify-content-around pt-5" action={{ route('equipe.store') }} method="post"
+            enctype="multipart/form-data">
             @csrf
-            <div class="mb-3">
-                <label class="form-label">Nom</label>
-                <input type="text" class="form-control" name="nom">
+            <div class="w-25">
+                <div class="mb-3">
+                    <label class="form-label">Nom</label>
+                    <input type="text" class="form-control" name="nom">
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Continent</label>
+                    <select name="continent_id" class="form-control">
+                        @foreach ($continents as $continent)
+                        <option value="{{$continent->nom}}">{{$continent->nom}}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Pays</label>
+                    <input type="text" class="form-control" name="pays">
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Ville</label>
+                    <input type="text" class="form-control" name="ville">
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Nombre de joueur : </label>
+                    <select name="max" class="form-control">
+                        <option value="1">1/7</option>
+                        <option value="2">2/7</option>
+                        <option value="3">3/7</option>
+                        <option value="4">4/7</option>
+                        <option value="5">5/7</option>
+                        <option value="6">6/7</option>
+                        <option value="7">7/7</option>
+                    </select>
+                </div>
             </div>
-            <div class="mb-3">
-                <label class="form-label">Ville</label>
-                <input type="text" class="form-control" name="nom">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Pays</label>
-                <input type="text" class="form-control" name="nom">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Continent</label>
-                <select name="continent_id" class="form-control">
-                    @foreach ($continents as $continent)
-                    <option value="{{$continent->nom}}">{{$continent->nom}}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Description</label>
-                <textarea class="form-control" name="description" cols="30" rows="10"></textarea>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">equipe</label>
-                <select name="equipe_id" class="form-control">
+
+            <div class="w-25">
+                <div class="mb-3">
+                    <label class="form-label">ATT</label>
+                    <select name="ATT" class="form-control">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">CT</label>
+                    <select name="CT" class="form-control">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">DC</label>
+                    <select name="DC" class="form-control">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">RP</label>
+                    <select name="RP" class="form-control">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                    </select>
+                </div>
 
 
-
-                </select>
+                <button type="submit" class="btn validate fs-1" style="margin-top: 20px"><i
+                        class="fas fa-check"></i></button>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </main>
